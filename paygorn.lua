8a863e9142041d9cc764b2470d62cfdd4f74732f
RecryDv <166589923+RecryDv@users.noreply.github.com>
2025-03-23 15:54:25 +0300

    Update paygorn.lua

--- paygorn.lua
+++ paygorn.lua
@@ -42,8 +42,8 @@ button.TextColor3 = Color3.fromRGB(0,0,0)
 button.Size = UDim2.fromScale(0.2, 0.1)
 
 local mbutton = Instance.new("TextButton", screen)
-mbutton.Position = UDim2.fromScale(0.5, 0.7)
-mbutton.AnchorPoint = Vector2.new(0.5, 0.7)
+mbutton.Position = UDim2.fromScale(0.5, 0.75)
+mbutton.AnchorPoint = Vector2.new(0.5, 0.725)
 mbutton.TextScaled = true
 mbutton.BackgroundColor3 = Color3.fromRGB(255,255,255)
 mbutton.TextColor3 = Color3.fromRGB(0,0,0)
@@ -68,15 +68,34 @@ spawn(function()
 			button.BackgroundColor3 = Color3.fromRGB(255, 90, 49)
 		end
 
-		mbutton.Text = mbutton.Text..tostring("("..rad.."x"..rad..")")
+		if typeof(rad) == "number" then
+			mbutton.Text = mbutton.Text..tostring(" ("..rad.."x"..rad..")")
+		elseif typeof(rad) == "string" then
+			mbutton.Text = mbutton.Text..tostring(" ("..rad..")")
+		end
+		
+		
 	end
 end)
+local mtime = {
+	Emerald = 0.05,
+	Amethyst = 0.5,
+	Rainbow = 1.5,
+}
 local cmd = require(game:GetService("ReplicatedStorage").Library.Client.MiningCmds)
 mbutton.MouseButton2Click:Connect(function()
-	if rad < 16 then
-		rad *= 2
-	elseif rad == 16 then
-		rad = 2
+	if not mining then
+		if typeof(rad) == "number" and rad < 16 then
+			rad *= 2
+		elseif rad == 16 then
+			rad = "Emerald"
+		elseif rad == "Emerald" then
+			rad = "Amethyst"
+		elseif rad == "Amethyst" then
+			rad = "Rainbow"
+		elseif rad == "Rainbow" then
+			rad = 2
+		end
 	end
 end)
 mbutton.MouseButton1Click:Connect(function()
@@ -88,7 +107,7 @@ mbutton.MouseButton1Click:Connect(function()
 
 
 	local foot = cmd.GetBlockAtFoot()
-	if foot ~= nil then
+	if foot ~= nil and typeof(rad) == "number" then
 		local pos = foot.Pos
 		local c = 0
 		local r2 = rad / 2
@@ -112,8 +131,55 @@ mbutton.MouseButton1Click:Connect(function()
 			end
 		end
 	end
+	
+	if typeof(rad) == "string" then
+		while mining do
+			task.wait()
+			local id = rad
 
-	if foot == nil then
+			local Blocks = nil
+
+			for i,v in pairs(workspace.__THINGS.BlockWorlds:GetChildren()) do
+				if v.Name:find("Blocks") then
+					Blocks = v
+				end
+			end
+			for i,v in pairs(Blocks:GetChildren()) do
+				if not mining then
+					break
+				end
+				local e = false
+				v.Destroying:Connect(function()
+					e = true
+				end)
+				if v:GetAttribute("id") == id then
+					repeat
+						task.wait()
+						local t = if id == "amethyst" then 0.5 else 1.5
+						game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(v.CFrame + Vector3.new(0, 0.5, 0))
+						local foot = cmd.GetBlockAtFoot()
+
+						local function dmg()
+							local foot = cmd.GetBlockAtFoot()
+							if foot ~= nil then
+								local pos = foot.Pos
+
+								game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("BlockWorlds_Target"):FireServer(pos)
+								task.wait(mtime[id])
+								game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("BlockWorlds_Break"):FireServer(pos)
+							end
+						end
+
+	
+
+						dmg()
+					until e == true or mining == false
+				end
+			end
+		end
+	end
+
+	if foot == nil and typeof(rad) == "number" then
 		mining = false
 	end
 end)
@@ -133,7 +199,7 @@ button.Activated:Connect(function()
 		end
 
 		local scanned = 0
-        local new = {}
+		local new = {}
 		for i,v in pairs(Blocks:GetChildren()) do
 			if math.random(1, 1250) == 1 then
 				task.wait()
@@ -165,10 +231,11 @@ button.Activated:Connect(function()
 				end
 			end)
 		end
-        for i,v in pairs(getgenv().cache.ores) do
+		
+		for i,v in pairs(getgenv().cache.ores) do
 			v:Destroy()
 		end
-        getgenv().cache.ores = new
+		getgenv().cache.ores = new
 		task.wait(0.2)
 	end
 end)
