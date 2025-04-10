52e195b0b63aa30faf0aeca47533333ea315ee31
RecryDv <166589923+RecryDv@users.noreply.github.com>
2025-04-11 01:53:24 +0300

    Update lol.lua

--- lol.lua
+++ lol.lua
@@ -45,7 +45,7 @@ Tabs.op:AddButton({
 	Callback = function()
 		local RebirthModule = require(game:GetService("Players").LocalPlayer.PlayerScripts.Client.Components.UI.Frames.RebirthFrame)
 		local unlocked = RebirthModule.canAutoRebirth()
-		
+
 		if unlocked == true then
 			client_not("Custom", "[sh0vel prod.] | Auto rebirth is already unlocked!", 3)
 			return
@@ -53,7 +53,7 @@ Tabs.op:AddButton({
 		hookfunction(RebirthModule.canAutoRebirth, function()
 			return true
 		end)
-		
+
 		client_not("Custom", "[sh0vel prod.] | Auto rebirth was unlocked!", 3)
 	end,
 })
@@ -62,10 +62,10 @@ Tabs.misc:AddToggle("123", {
 	Title = "Auto collect orbs",
 	Callback = function(val)
 		d.ao = val
-		
+
 		while d.ao do
 			task.wait(0.01)
-			
+
 			for _, orb in pairs(workspace.Debris.Orbs:GetChildren()) do
 				orb.CFrame = player.Character.PrimaryPart.CFrame
 			end
@@ -77,7 +77,7 @@ Tabs.misc:AddToggle("123", {
 	Title = "Auto collect chest",
 	Callback = function(val)
 		d.ac = val
-		
+
 		while d.ac do
 			task.wait()
 			for _, world in pairs(workspace.Game.Maps:GetChildren()) do
@@ -124,7 +124,7 @@ local fucking_dropdown = Tabs.aw:AddDropdown("123", {
 	Values = trees_loc,
 	Multi = true,
 	Default = {},
-	
+
 	Callback = function(val)
 		d.aw_loc = val
 	end,
@@ -143,30 +143,30 @@ fucking_dropdown:SetValue(frmtd)
 
 Tabs.aw:AddButton({
 	Title = "Define lumber id",
-	
+
 	Callback = function()
 		if d.L_id ~= "" then
 			client_not("Custom", "[sh0vel prod.] | Lumber id is already defined!", 3)
 			return
 		end
-		
+
 		client_not("Custom", "[sh0vel prod.] | Cut any wood!", 5)
 		local possible = ""
-		
+
 		local oldcall
-		
+
 		oldcall = hookmetamethod(game, "__namecall", function(self, ...)
-			
+
 			local Args = {...}
 			local Method = getnamecallmethod()
-			
+
 			if Method == "FireServer" and #Args == 3 and typeof(Args[3]) == "string" then
 				possible = Args[3]
 			end
-			
+
 			return oldcall(self, ...)
 		end)
-		
+
 		repeat
 			task.wait()
 		until possible ~= ""
@@ -180,7 +180,7 @@ Tabs.aw:AddToggle("123", {
 	Callback = function(val)
 		d.aw = val
 		client_not("Custom", "[sh0vel prod.] | Wood Cutting "..generate_toggle(val), 0.5)
-		
+
 		local function isbroken(tree)
 			local broken = false
 			for i,v in pairs(tree:FindFirstChildOfClass("Model"):GetChildren()) do
@@ -189,23 +189,23 @@ Tabs.aw:AddToggle("123", {
 						broken = true
 					end
 				end)
-				
+
 			end
 
-            pcall(function()
-            for i,v in pairs(tree:FindFirstChildOfClass("Model").PrimaryPart:GetChildren()) do
-				pcall(function()
-					if v.Enabled == true then
-						broken = true
-					end
-				end)
-				
-			end
-            end)
-			
+			pcall(function()
+				for i,v in pairs(tree:FindFirstChildOfClass("Model").PrimaryPart:GetChildren()) do
+					pcall(function()
+						if v.Enabled == true then
+							broken = true
+						end
+					end)
+
+				end
+			end)
+
 			return broken
 		end
-		
+
 		if d.aw then
 			if d.aw_loc == {} or d.aw_loc == nil then
 				client_not("Custom", "[sh0vel prod.] | Please select location!", 5)
@@ -216,47 +216,47 @@ Tabs.aw:AddToggle("123", {
 				client_not("Custom", "[sh0vel prod.] | Equip your axe first!", 5)
 				return
 			end
-			
+
 			if d.L_id == "" then
 				client_not("Custom", "[sh0vel prod.] | Unknown lumber id. Please click button 'Define lumber id'", 5)
 				return
 			end
 		end
-		
-		
+
+
 		while d.aw do
-        local client_tree = ""
+			local client_tree = ""
 			for loc, val in pairs(d.aw_loc) do
 				pcall(function()  
-                if val then
-					task.wait()
-					local trees = workspace.Game.Maps[loc].Trees:GetChildren()
-					local tree = trees[math.random(1, #trees)]
-					client_tree = tostring(game:GetService("HttpService"):GenerateGUID(false))
-					local current = client_tree
-					if not isbroken(tree) then
-						local root = tree:FindFirstChildOfClass("Model")
+					if val then
+						task.wait()
+						local trees = workspace.Game.Maps[loc].Trees:GetChildren()
+						local tree = trees[math.random(1, #trees)]
+						client_tree = tostring(game:GetService("HttpService"):GenerateGUID(false))
+						local current = client_tree
+						if not isbroken(tree) then
+							local root = tree:FindFirstChildOfClass("Model")
 
-						client_not("Custom", "[sh0vel prod.] | Teleporting to tree.", 2.5)
+							client_not("Custom", "[sh0vel prod.] | Teleporting to tree.", 2.5)
 
-						
 
-						repeat
-                        game.Players.LocalPlayer.Character.PrimaryPart.CFrame = root.PrimaryPart.CFrame
-							
-							task.spawn(function()
-                            TreeService.damage2:Fire(
-								tree:GetAttribute("groupId"),
-								tree:GetAttribute("treeId"),
-								d.L_id
-							)
-                            end)
-							task.wait()
-						until not d.aw or isbroken(tree)
-						client_not("Custom", "[sh0vel prod.] | Done with tree "..client_tree, 2.5)
+
+							repeat
+								game.Players.LocalPlayer.Character.PrimaryPart.CFrame = root.PrimaryPart.CFrame
+
+								task.spawn(function()
+									TreeService.damage2:Fire(
+										tree:GetAttribute("groupId"),
+										tree:GetAttribute("treeId"),
+										d.L_id
+									)
+								end)
+								task.wait()
+							until not d.aw or isbroken(tree)
+							client_not("Custom", "[sh0vel prod.] | Done with tree "..client_tree, 2.5)
+						end
 					end
-				end
-                end)
+				end)
 
 			end
 		end
@@ -264,3 +264,11 @@ Tabs.aw:AddToggle("123", {
 })
 
 client_not("Custom", "[sh0vel prod.] | Injected!",  5)
+
+task.spawn(function()
+	while task.wait(0.01) do
+		game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.Unknown, false, game.Players.LocalPlayer.PlayerGui)
+		task.wait(0.01)
+		game:GetService("VirtualInputManager"):SendKeyEvent(false, Enum.KeyCode.Unknown, false, game.Players.LocalPlayer.PlayerGui)
+	end
+end)
